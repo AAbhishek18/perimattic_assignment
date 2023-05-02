c69cdfdd9026b78d4af5a4de878661a4a3016c11
Abhishek Kumar <kumarabhishek0947@gmail.com>
2023-05-03 02:47:37 +0530

    perimattic_assignment

--- index.js
+++ index.js
@@ -3,10 +3,17 @@ const express = require('express');
 const app = express();
 const mysql = require('mysql');
 const bodyParser = require('body-parser');
-//const cors = require('cors');
 
-// app.use(cors());
-// app.use(bodyParser.json());
+
+const path = require('path');
+const multer = require('multer')
+const upload = multer({ dest: 'uploads/' })
+
+const axios =require('axios')
+
+
+
+ //app.use(bodyParser.json());
 
 //create connection
 
@@ -16,6 +23,7 @@ const db = mysql.createConnection({
     host: 'localhost',
     user: 'root',
     password: 'root',
+    database: 'perimattic_assignment'
     
 });
 
@@ -31,27 +39,116 @@ db.connect((err) => {
     console.log('mysql connected');
 });
 
+//parse the json body
+app.use(express.json());
 //
+
+
+// app.use('/api',route)
+
+
+
 app.listen(process.env.PORT||3000,()=>{
     console.log(`Server is running on Poert ${process.env.PORT||3000} `)
 } )
 
 
 
-//create database
 
-
-app.get('/createdb', (req,res) => {
-
-    let sql = 'CREATE DATABASE perimattic_assignment';
-    db.query(sql, (err,result) => {
-        if(err) throw err;
-        console.log(result);
-        res.send('database created');
+//upload data to mysql database=============================
+app.get('/getdata', async function (req, res)  {
+    const data = await axios.get('https://api.publicapis.org/entries')
+    console.log(data.data.entries)
+    const {entries} = data.data
+    entries.forEach(async (element) => {
+        const {API,Description,Auth,HTTPS,Cors,Link,Category} = element
+        const data = await db.query("INSERT INTO api set API =? ,Description=?,Auth=?,HTTPS=?,Cors=?,Link=?,Category=?",[API,Description,Auth,HTTPS,Cors,Link,Category]);
     });
-}
+    res.send({})
 
-);
+})
 
-//create table
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//=======================================================================
+app.post('/insertUser', (req, res) => {
+    // Get data from the request body
+    console.log(req.body)
+    const { name, email, phone, address } = req.body;
+  
+    // Insert data into the USER table
+    const data = { name, email, phone, address };
+    db.query('INSERT INTO USER SET ?', data, (err, result) => {
+      if (err) throw err;
+      console.log('Data inserted into USER table!');
+  
+      // Send response to client
+      res.status(201).send('User created!');
+    });
+  });
+
+
+  // get USERS===================================================================
+    app.get('/getUsers', (req, res) => {
+        // Get data from the database
+        db.query('SELECT * FROM USER', (err, result) => {
+            if (err) throw err;
+            console.log(result);
+            res.send(result);
+        });
+    });
+
+
+    // get USER by id===================================================================
+    app.get('/getUser/:id', (req, res) => {
+        // Get data from the database
+        db.query('SELECT * FROM USER WHERE id = ?', [req.params.id], (err, result) => {
+            if (err) throw err;
+            console.log(result);
+            res.send(result);
+        });
+    });
+
+
+    // update USER by id==================================================================
+    app.put('/updateUser/:id', (req, res) => {
+        // Get data from the request body
+        const { name, email, phone, address } = req.body;
+        // Get data from the database
+        db.query(
+            'UPDATE USER SET name = ?, email = ?, phone = ?, address = ? WHERE id = ?',
+            [name, email, phone, address, req.params.id],
+            (err, result) => {
+                if (err) throw err;
+                console.log(result);
+                res.send(result);
+            }
+        );
+            
+    });
+
+
+    // delete USER by id============================================================
+    app.delete('/deleteUser/:id', (req, res) => {
+        // Get data from the database
+        db.query('DELETE FROM USER WHERE id = ?', [req.params.id], (err, result) => {
+            if (err) throw err;
+            console.log(result);
+            res.send(result);
+        });
+    });
+
 
